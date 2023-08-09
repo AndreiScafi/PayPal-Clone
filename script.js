140c73c8b3dec99a81208d4426979a1e5e8097a1
Andrei Scafi <andreiscafi@hotmail.com>
2023-08-09 03:40:02 -0300

    Professor's solution for nav-dropdown with Javascript.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 /* My Own Solution */
 /* Navigation */
+/* 
 const dropdown = document.querySelectorAll('.dropdown-hover');
 
 dropdown.forEach(element => {
@@ -12,6 +13,27 @@ dropdown.forEach(element => {
     element.addEventListener('mouseout', () => {
         element.classList.remove('change');
     })
-})
+}) 
+*/
 /* End of Navigation */
 /* End of My Own Solution */
+
+/* Professor's Solution */
+
+const dropdownItems = document.querySelectorAll('.dropdown-hover');
+
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener('mouseover', () => {
+        dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible';
+        document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)';
+    })
+})
+
+dropdownItems.forEach(dropdownItem => {
+    dropdownItem.addEventListener('mouseout', () => {
+        dropdownItem.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden';
+        document.querySelector('.navbar-wrapper').style.background = 'transparent';
+    })
+})
+
+/* End of Professor's Solution */
