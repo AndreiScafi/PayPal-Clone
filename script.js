b1aa5631d64a0d011f4f4c89522dcea02bf499a6
Andrei Scafi <andreiscafi@hotmail.com>
2023-08-14 14:44:51 -0300

    My own solution for changing pages functionality with javascript.

--- script.js
+++ script.js
@@ -19,6 +19,44 @@ dropdown.forEach(element => {
 })
 
 /* End of Navigation */
+/* Change Pages */
+
+const login = document.querySelectorAll('.login');
+const signup = document.querySelectorAll('.signup');
+const logo = document.querySelectorAll('.logo');
+const frontPage = document.querySelector('.front-page');
+const loginPage = document.querySelector('.login-page');
+const signupPage = document.querySelector('.signup-page');
+
+
+login.forEach(item => {
+    item.addEventListener('click', () => {
+        frontPage.style.display = 'none';
+        signupPage.style.display = 'none';
+        loginPage.style.display = 'block';
+    })
+});
+
+signup.forEach(item => {
+    item.addEventListener('click', () => {
+        frontPage.style.display = 'none';
+        loginPage.style.display = 'none';
+        signupPage.style.display = 'flex';
+
+    })
+});
+
+logo.forEach(item => {
+    item.addEventListener('click', () => {
+        frontPage.style.display = 'block';
+        loginPage.style.display = 'none';
+        signupPage.style.display = 'none';
+        console.log('clicked')
+
+    })
+});
+
+/* End of Change Pages */
 /* End of My Own Solution */
 
 /* Professor's Solution */
